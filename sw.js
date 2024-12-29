a2dfc30b9daa667120500645fab6e5b627df8601
John Doe <johndoe@example.com>
2024-12-29 15:37:07 +0100

    v1

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'hello-pwa-v1';
+const CACHE_ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(CACHE_ASSETS);
+            })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+}); 
